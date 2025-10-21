e515063b996a0a8675d2378c8d0af35938cf3fe1
miniotti <robin.miniotti@etu.unistra.fr>
2025-10-21 19:07:45 +0200

    feat: created pollution service + environments

---
+++ tp03/src/environments/pollution-details/pollution-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pollution-details',
+  imports: [],
+  templateUrl: './pollution-details.component.html',
+  styleUrl: './pollution-details.component.css'
+})
+export class PollutionDetailsComponent {
+
+}
---
+++ tp03/src/environments/pollution-list/pollution-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pollution-list',
+  imports: [],
+  templateUrl: './pollution-list.component.html',
+  styleUrl: './pollution-list.component.css'
+})
+export class PollutionListComponent {
+
+}
